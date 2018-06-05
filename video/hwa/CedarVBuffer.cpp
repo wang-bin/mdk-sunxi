1bbe27539ef09e3749c12c4049f0eea13363154d
wang-bin <wbsecg1@gmail.com>
2018-06-05 22:53:25 +0800

    glva.sunxi: upload tiled image directly. test only

--- video/hwa/CedarVBuffer.cpp
+++ video/hwa/CedarVBuffer.cpp
@@ -4,6 +4,9 @@
 #include "mdk/VideoBuffer.h"
 #include "mdk/VideoFrame.h"
 #include "NativeVideoBufferTemplate.h"
+#include "video/opengl/GLGlue.h"
+#include "ugl/gl_api.h" // egl_api.h is included if HAVE_EGL_CAPI is defined
+#include "ugl/context.h"
 #include <iostream>
 #include <memory>
 #include <mutex>
@@ -11,18 +14,46 @@ extern "C" {
 #include <libcedarv/libcedarv.h>
 }
 using namespace std;
+using namespace UGL_NS::opengl;
 #define FFALIGN(x, a) (((x)+(a)-1)&~((a)-1))
 
 MDK_NS_BEGIN
 class CedarVBufferPool final : public NativeVideoBufferPool {
 public:
     NativeVideoBufferRef getBuffer(void* opaque, std::function<void()> cleanup = nullptr) override;
-    bool transfer_to_host(cedarv_picture_t* buf, NativeVideoBuffer::MemoryArray* ma, NativeVideoBuffer::MapParameter *mp);
-    bool transfer_begin(cedarv_picture_t* buf, NativeVideoBuffer::GLTextureArray* ma, NativeVideoBuffer::MapParameter *mp) {return false;}
+    bool transfer_begin(cedarv_picture_t* buf, NativeVideoBuffer::GLTextureArray* ma, NativeVideoBuffer::MapParameter *mp);
     void transfer_end() {}
+    bool transfer_to_host(cedarv_picture_t* buf, NativeVideoBuffer::MemoryArray* ma, NativeVideoBuffer::MapParameter *mp);
 private:
+    Context* updateContext() {
+        Context* c = Context::current();
+        if (c == ctx_)
+            return c;
+        ctx_ = c;
+        ctx_res_ = nullptr;
+        return c;
+    }
+
+    bool ensureGL(const VideoFormat& fmt, int* w, int* h);
+    struct ctx_res_t {
+        GLuint tex[2] = {};
+        GLenum format[4]{};
+        GLenum data_type[4]{};
+        int count = 2;
+    };
+    Context *ctx_ = nullptr; // used to check context change
+    ctx_res_t* ctx_res_ = nullptr; // current cls value used
+
     std::mutex hos_mutex_;
     VideoFrame host_;
+
+    Context::Local<ctx_res_t> res = {[](ctx_res_t& r){
+        std::clog << "release CedarV-GL interop resources" << std::endl;
+        for (auto& t : r.tex) {
+            if (t)
+                gl().DeleteTextures(t, &t);
+        }
+    }};
 };
 
 typedef shared_ptr<CedarVBufferPool> PoolRef;
@@ -31,6 +62,63 @@ using CedarVBuffer = NativeVideoBufferImpl<cedarv_picture_t*, CedarVBufferPool>;
 NativeVideoBufferRef CedarVBufferPool::getBuffer(void* opaque, std::function<void()> cleanup)
 {
     return std::make_shared<CedarVBuffer>(static_pointer_cast<CedarVBufferPool>(shared_from_this()), static_cast<cedarv_picture_t*>(opaque), cleanup);    
+}
+
+
+bool CedarVBufferPool::ensureGL(const VideoFormat& fmt, int* w, int* h)
+{
+    if (ctx_res_->tex[0])
+        return true;
+    ctx_res_->count = fmt.planeCount();
+    GLint tex_ifmt[4]{};
+    toGL(fmt, tex_ifmt, ctx_res_->format, ctx_res_->data_type);
+    const auto& gl = UGL_NS::opengl::gl(); // *ctx_->gl()
+    gl.GenTextures(ctx_res_->count, ctx_res_->tex);
+    std::clog << "CedarV-GL interop: upload tiled image to gl texture directly" << std::endl;
+    for (size_t i = 0; i < ctx_res_->count; ++i) {
+        GLuint& t = ctx_res_->tex[i];
+        gl.BindTexture(GL_TEXTURE_2D, t);
+        if (w[i] <= 0)
+            w[i] = fmt.width(w[0], i);
+        if (h[i] <= 0)
+            h[i] = fmt.height(h[0], i);
+        gl.TexImage2D(GL_TEXTURE_2D, 0, tex_ifmt[i], w[i], h[i], 0, ctx_res_->format[i], ctx_res_->data_type[i], nullptr);
+        gl.TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        gl.TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        gl.TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); //GL_NEAREST?
+        gl.TexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        gl.BindTexture(GL_TEXTURE_2D, 0);
+    }
+    return true;
+}
+
+bool CedarVBufferPool::transfer_begin(cedarv_picture_t* buf, NativeVideoBuffer::GLTextureArray* ma, NativeVideoBuffer::MapParameter *mp)
+{
+    if (ma->id[0] > 0)
+        return true;
+    if (!updateContext())
+        return false;
+    if (!ctx_res_) {
+        ctx_res_ = &res.get(ctx_);
+    }
+    const VideoFormat fmt = PixelFormat::NV12;
+    buf->display_height = FFALIGN(buf->display_height, 8);
+    mp->width[0] = FFALIGN(buf->display_width, 16);
+    mp->height[0] = FFALIGN(buf->display_height, 2); // already aligned to 8!
+    if (!ensureGL(fmt, mp->width, mp->height))
+        return false;
+    const auto& gl = *ctx_->gl();
+    const void* bits[] = {buf->y, buf->u};
+    for (int i = 0; i < ctx_res_->count; ++i) {
+        gl.BindTexture(GL_TEXTURE_2D, ctx_res_->tex[i]);
+        gl.TexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, fmt.width(mp->width[0], i), fmt.height(mp->height[0], i), ctx_res_->format[i], ctx_res_->data_type[i], bits[i]);
+        gl.BindBuffer(GL_PIXEL_UNPACK_BUFFER, 0);
+        ma->id[i] = ctx_res_->tex[i];
+        //printf("ma->id[%d]: %d, %dx%d @%p\n", i, ma->id[i], mp->width[i], mp->height[i], bits[i]);
+    }
+    ma->target = GL_TEXTURE_2D;
+    mp->format = fmt;
+    return true;
 }
 
 // source data is colum major. every block is 32x32
@@ -115,7 +203,8 @@ map_c_t map_c_ = map32x32_to_yuv_C;
 
 bool CedarVBufferPool::transfer_to_host(cedarv_picture_t* buf, NativeVideoBuffer::MemoryArray* ma, NativeVideoBuffer::MapParameter *mp)
 {
-    // TODO: upload as texture directly and convert tile in shader
+    if (ma->data[0]) // can be reused
+        return true;
     buf->display_height = FFALIGN(buf->display_height, 8);
     const int display_h_align = FFALIGN(buf->display_height, 2); // already aligned to 8!
     const int display_w_align = FFALIGN(buf->display_width, 16);
